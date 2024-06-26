fc486890da7ce128c0bbf417694b954579218b14
teddyrendahl <trendahl@covariant.ai>
2024-06-26 10:46:01 -0700

    Put all forward pass in a single Layer

--- src/mlp.rs
+++ src/mlp.rs
@@ -13,11 +13,12 @@ const EMBEDDING_SIZE: i64 = 5;
 const CHUNK_SIZE: i64 = 3;
 const HIDDEN_LAYER_SIZE: i64 = 100;
 const BATCH_SIZE: i64 = 32;
+const VOCAB_SIZE: i64 = 27;
 
 trait Layer {
     fn forward(&mut self, x: tch::Tensor) -> tch::Tensor;
-    fn zero_grad(&mut self);
-    fn backward(&mut self, learning_rate: f64);
+    fn zero_grad(&mut self) {}
+    fn backward(&mut self, _learning_rate: f64) {}
     fn set_training(&mut self, _training: bool) {}
 }
 
@@ -28,8 +29,9 @@ struct Linear {
 
 impl Linear {
     pub fn new(fan_in: i64, fan_out: i64, bias: bool, device: tch::Device) -> Self {
-        let weight =
+        let mut weight =
             tch::Tensor::randn([fan_in, fan_out], (Kind::Float, device)).set_requires_grad(true);
+        tch::no_grad(|| weight /= (fan_in as f32).sqrt());
         let bias_layer = if bias {
             let b = tch::Tensor::randn([fan_out], (Kind::Float, device)).set_requires_grad(true);
             Some(b)
@@ -131,6 +133,34 @@ impl Layer for BatchNorm1D {
     }
 }
 
+struct Sequential {
+    layers: Vec<Box<dyn Layer>>,
+}
+
+impl Layer for Sequential {
+    fn forward(&mut self, x: tch::Tensor) -> tch::Tensor {
+        self.layers.iter_mut().fold(x, |acc, l| l.forward(acc))
+    }
+
+    fn zero_grad(&mut self) {
+        for l in self.layers.iter_mut() {
+            l.zero_grad()
+        }
+    }
+
+    fn backward(&mut self, learning_rate: f64) {
+        for l in self.layers.iter_mut() {
+            l.backward(learning_rate)
+        }
+    }
+
+    fn set_training(&mut self, training: bool) {
+        for l in self.layers.iter_mut() {
+            l.set_training(training)
+        }
+    }
+}
+
 struct Tanh {}
 
 impl Tanh {
@@ -143,11 +173,42 @@ impl Layer for Tanh {
     fn forward(&mut self, x: tch::Tensor) -> tch::Tensor {
         x.tanh()
     }
-
-    fn zero_grad(&mut self) {}
-    fn backward(&mut self, _learning_rate: f64) {}
 }
 
+struct Embedding {
+    weight: tch::Tensor,
+}
+
+impl Embedding {
+    pub fn new(num_embeddings: i64, embedding_dim: i64, device: tch::Device) -> Self {
+        Embedding {
+            weight: tch::Tensor::randn([num_embeddings, embedding_dim], (Kind::Float, device))
+                .requires_grad_(true),
+        }
+    }
+}
+
+impl Layer for Embedding {
+    fn forward(&mut self, x: tch::Tensor) -> tch::Tensor {
+        self.weight.index(&[Some(x)])
+    }
+
+    fn zero_grad(&mut self) {
+        self.weight.zero_grad()
+    }
+
+    fn backward(&mut self, learning_rate: f64) {
+        self.weight += self.weight.grad() * -learning_rate
+    }
+}
+
+struct Flatten {}
+
+impl Layer for Flatten {
+    fn forward(&mut self, x: tch::Tensor) -> tch::Tensor {
+        x.view([x.size()[0], -1])
+    }
+}
 fn main() -> Result<()> {
     tch::manual_seed(SEED);
     let device = tch::Device::cuda_if_available();
@@ -162,62 +223,38 @@ fn main() -> Result<()> {
     let n2 = (names.len() as f32 * 0.9) as usize;
 
     let (xtr, ytr) = load_data(&names[0..n1], &c_to_i, CHUNK_SIZE as usize);
-    let (_xdev, _ydev) = load_data(&names[n1..n2], &c_to_i, CHUNK_SIZE as usize);
+    let (xdev, ydev) = load_data(&names[n1..n2], &c_to_i, CHUNK_SIZE as usize);
     let (_xte, _yte) = load_data(&names[n2..], &c_to_i, CHUNK_SIZE as usize);
 
-    let mut c =
-        tch::Tensor::randn([27, EMBEDDING_SIZE], (Kind::Float, device)).requires_grad_(true);
+    let mut layers: Vec<Box<dyn Layer>> = vec![
+        Box::new(Embedding::new(VOCAB_SIZE, EMBEDDING_SIZE, device)),
+        Box::new(Flatten {}),
+        Box::new(Linear::new(
+            EMBEDDING_SIZE * CHUNK_SIZE,
+            HIDDEN_LAYER_SIZE,
+            false,
+            device,
+        )),
+        Box::new(BatchNorm1D::new(HIDDEN_LAYER_SIZE, 1e-5, 0.1, device)),
+        Box::new(Tanh::new()),
+    ];
 
-    let mut layers: Vec<Box<dyn Layer>> = [
-        (EMBEDDING_SIZE * CHUNK_SIZE, HIDDEN_LAYER_SIZE),
-        (HIDDEN_LAYER_SIZE, HIDDEN_LAYER_SIZE),
-        (HIDDEN_LAYER_SIZE, HIDDEN_LAYER_SIZE),
-        (HIDDEN_LAYER_SIZE, HIDDEN_LAYER_SIZE),
-        (HIDDEN_LAYER_SIZE, HIDDEN_LAYER_SIZE),
-    ]
-    .into_iter()
-    .flat_map(|(f_in, f_out)| {
-        let mut linear = Linear::new(f_in, f_out, false, device);
-        let _g = tch::no_grad_guard();
-        linear.weight *= (5.0 / 3.0) / (EMBEDDING_SIZE as f32 * CHUNK_SIZE as f32).sqrt();
-        [
-            Box::new(linear) as Box<dyn Layer>,
-            Box::new(BatchNorm1D::new(HIDDEN_LAYER_SIZE, 1e-5, 0.1, device)),
-            Box::new(Tanh::new()) as _,
-        ]
-    })
-    .collect();
-
-    let mut last_linear = Linear::new(HIDDEN_LAYER_SIZE, 27, true, device);
-    {
-        let _g = tch::no_grad_guard();
-        last_linear.weight *= 0.1;
-    }
+    let mut last_linear = Linear::new(HIDDEN_LAYER_SIZE, 27, false, device);
+    tch::no_grad(|| last_linear.weight *= 0.1);
     layers.push(Box::new(last_linear));
+
+    let mut model = Sequential { layers };
     for i in 0..200_000 {
         let ix = tch::Tensor::randint(xtr.size()[0], [BATCH_SIZE], (Kind::Int, device));
         let samples = xtr.i(&ix);
         let labels = ytr.i(&ix);
-        let emb = c.index(&[Some(samples)]);
-        let mut x = emb.view_([-1, CHUNK_SIZE * EMBEDDING_SIZE]);
-
-        for layer in &mut layers {
-            x = layer.forward(x);
-        }
-        let loss = x.cross_entropy_for_logits(&labels);
-        c.zero_grad();
-        for l in &mut layers {
-            l.zero_grad()
-        }
+        let logits = model.forward(samples);
+        let loss = logits.cross_entropy_for_logits(&labels);
+        model.zero_grad();
         loss.backward();
         let lr = if i < 100_000 { 0.1 } else { 0.01 };
 
-        tch::no_grad(|| {
-            c += c.grad() * (-lr);
-            for l in &mut layers {
-                l.backward(lr)
-            }
-        });
+        tch::no_grad(|| model.backward(lr));
 
         if i % 10_000 == 0 {
             loss.print()
@@ -226,22 +263,18 @@ fn main() -> Result<()> {
 
     tch::manual_seed(SEED + 10);
 
-    for layer in layers.iter_mut() {
-        layer.set_training(false);
-    }
+    model.set_training(false);
+    println!("Training loss -> {}", split_loss(xtr, &mut model, &ytr));
+    println!("Validation loss -> {}", split_loss(xdev, &mut model, &ydev));
 
     // Generate a name by repeatedly sampling from the probability distribution of bigrams
     // until an end token is found.
     for _ in 0..20 {
         let mut out = Vec::new();
-        let mut ctx = tch::Tensor::zeros([CHUNK_SIZE], (Kind::Int, device));
+        let mut ctx = tch::Tensor::zeros(CHUNK_SIZE, (Kind::Int, device));
         loop {
-            let emb = c.index(&[Some(&ctx)]);
-            let mut x = emb.view_([1, -1]);
-            for l in &mut layers {
-                x = l.forward(x);
-            }
-            let prob = x.softmax(1, Kind::Float);
+            let logits = model.forward(ctx.view([1, 3]));
+            let prob = logits.softmax(1, Kind::Float);
             let idx = prob.multinomial(1, true).get(0).int64_value(&[0]);
             out.push(i_to_c[&(idx as usize)]);
             if idx == 0 {
@@ -300,4 +333,10 @@ fn load_data(
         tch::Tensor::from_slice2(&xs).to_kind(Kind::Int),
         tch::Tensor::from_slice(&ys).to_kind(tch::Kind::Int64),
     )
+}
+
+fn split_loss(data: tch::Tensor, model: &mut dyn Layer, labels: &tch::Tensor) -> f64 {
+    let _g = tch::no_grad_guard();
+    let logits = model.forward(data);
+    f64::try_from(logits.cross_entropy_for_logits(labels)).unwrap()
 }
