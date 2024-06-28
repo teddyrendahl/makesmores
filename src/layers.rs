237202ff1504c05c10328b7b5c9ebd1df5cce60f
teddyrendahl <trendahl@covariant.ai>
2024-06-27 17:36:30 -0700

    Move models to main

--- src/layers.rs
+++ src/layers.rs
@@ -1,30 +1,17 @@
-use std::{collections::{BTreeMap, HashMap}, vec};
+use std::vec;
 
-use anyhow::Result;
-use itertools::Itertools;
-use rand::seq::SliceRandom;
-use rand::SeedableRng;
-use rand_chacha::ChaCha12Rng;
-use tch::{IndexOp, Kind};
+use tch::Kind;
 
-const EDGE_TOKEN: char = '.';
-const SEED: i64 = 2147483647;
-const EMBEDDING_SIZE: i64 = 24;
-const CHUNK_SIZE: i64 = 8;
-const HIDDEN_LAYER_SIZE: i64 = 128;
-const BATCH_SIZE: i64 = 32;
-const VOCAB_SIZE: i64 = 27;
-
-trait Layer {
+pub(crate) trait Layer {
     fn forward(&mut self, x: tch::Tensor) -> tch::Tensor;
     fn zero_grad(&mut self) {}
     fn backward(&mut self, _learning_rate: f64) {}
     fn set_training(&mut self, _training: bool) {}
 }
 
-struct Linear {
-    weight: tch::Tensor,
-    bias: Option<tch::Tensor>,
+pub(crate) struct Linear {
+    pub weight: tch::Tensor,
+    pub bias: Option<tch::Tensor>,
 }
 
 impl Linear {
@@ -71,7 +58,7 @@ impl Layer for Linear {
     }
 }
 
-struct BatchNorm1D {
+pub(crate) struct BatchNorm1D {
     eps: f64,
     momentum: f32,
     gamma: tch::Tensor,
@@ -101,9 +88,9 @@ impl Layer for BatchNorm1D {
             let dim = match x.dim() {
                 2 => vec![0],
                 3 => vec![0, 1],
-                d => panic!("Unsupported vector size {}", d)
+                d => panic!("Unsupported vector size {}", d),
             };
-            let xmean = x.mean_dim(&dim, true,Some(Kind::Float));
+            let xmean = x.mean_dim(&dim, true, Some(Kind::Float));
             let xvar = x.var_dim(&dim, true, true);
             (xmean, xvar)
         } else {
@@ -138,8 +125,8 @@ impl Layer for BatchNorm1D {
     }
 }
 
-struct Sequential {
-    layers: Vec<Box<dyn Layer>>,
+pub(crate) struct Sequential {
+    pub layers: Vec<Box<dyn Layer>>,
 }
 
 impl Layer for Sequential {
@@ -166,7 +153,7 @@ impl Layer for Sequential {
     }
 }
 
-struct Tanh {}
+pub(crate) struct Tanh {}
 
 impl Tanh {
     pub fn new() -> Self {
@@ -180,7 +167,7 @@ impl Layer for Tanh {
     }
 }
 
-struct Embedding {
+pub(crate) struct Embedding {
     weight: tch::Tensor,
 }
 
@@ -207,8 +194,8 @@ impl Layer for Embedding {
     }
 }
 
-struct FlattenConsecutive {
-    n: i64,
+pub(crate) struct FlattenConsecutive {
+    pub n: i64,
 }
 
 impl Layer for FlattenConsecutive {
@@ -221,152 +208,69 @@ impl Layer for FlattenConsecutive {
     }
 }
 
-fn main() -> Result<()> {
-    tch::manual_seed(SEED);
-    let device = tch::Device::cuda_if_available();
-    let mut names = get_names()?;
-    // Generate a mapping of token to integer (and the reverse)
-    let (c_to_i, i_to_c) = create_char_maps(&names);
-
-    // Split dataset into train, dev, test
-    let mut rng = ChaCha12Rng::seed_from_u64(42);
-    names.shuffle(&mut rng);
-    let n1 = (names.len() as f32 * 0.8) as usize;
-    let n2 = (names.len() as f32 * 0.9) as usize;
-    tch::nn::Linear
-    let (xtr, ytr) = load_data(&names[0..n1], &c_to_i, CHUNK_SIZE as usize);
-    let (xdev, ydev) = load_data(&names[n1..n2], &c_to_i, CHUNK_SIZE as usize);
-    let (_xte, _yte) = load_data(&names[n2..], &c_to_i, CHUNK_SIZE as usize);
-
+pub(crate) fn create_wavenet(
+    device: tch::Device,
+    vocab_size: i64,
+    embedding_size: i64,
+    hidden_layer_size: i64,
+) -> Sequential {
     let mut layers: Vec<Box<dyn Layer>> = vec![
-        Box::new(Embedding::new(VOCAB_SIZE, EMBEDDING_SIZE, device)),
+        Box::new(Embedding::new(vocab_size, embedding_size, device)),
         Box::new(FlattenConsecutive { n: 2 }),
         Box::new(Linear::new(
-            EMBEDDING_SIZE * 2,
-            HIDDEN_LAYER_SIZE,
+            embedding_size * 2,
+            hidden_layer_size,
             false,
             device,
         )),
-        Box::new(BatchNorm1D::new(HIDDEN_LAYER_SIZE, 1e-5, 0.1, device)),
+        Box::new(BatchNorm1D::new(hidden_layer_size, 1e-5, 0.1, device)),
         Box::new(Tanh::new()),
         Box::new(FlattenConsecutive { n: 2 }),
         Box::new(Linear::new(
-            HIDDEN_LAYER_SIZE * 2,
-            HIDDEN_LAYER_SIZE,
+            hidden_layer_size * 2,
+            hidden_layer_size,
             false,
             device,
         )),
-        Box::new(BatchNorm1D::new(HIDDEN_LAYER_SIZE, 1e-5, 0.1, device)),
+        Box::new(BatchNorm1D::new(hidden_layer_size, 1e-5, 0.1, device)),
         Box::new(Tanh::new()),
         Box::new(FlattenConsecutive { n: 2 }),
         Box::new(Linear::new(
-            HIDDEN_LAYER_SIZE * 2,
-            HIDDEN_LAYER_SIZE,
+            hidden_layer_size * 2,
+            hidden_layer_size,
             false,
             device,
         )),
-        Box::new(BatchNorm1D::new(HIDDEN_LAYER_SIZE, 1e-5, 0.1, device)),
+        Box::new(BatchNorm1D::new(hidden_layer_size, 1e-5, 0.1, device)),
         Box::new(Tanh::new()),
     ];
-
-    let mut last_linear = Linear::new(HIDDEN_LAYER_SIZE, 27, false, device);
+    let mut last_linear = Linear::new(hidden_layer_size, vocab_size, false, device);
     tch::no_grad(|| last_linear.weight *= 0.1);
     layers.push(Box::new(last_linear));
-
-    let mut model = Sequential { layers };
-    for i in 0..200_000 {
-        let ix = tch::Tensor::randint(xtr.size()[0], [BATCH_SIZE], (Kind::Int, device));
-        let samples = xtr.i(&ix);
-        let labels = ytr.i(&ix);
-        let logits = model.forward(samples);
-        let loss = logits.cross_entropy_for_logits(&labels);
-        model.zero_grad();
-        loss.backward();
-        let lr = if i < 100_000 { 0.1 } else { 0.01 };
-
-        tch::no_grad(|| model.backward(lr));
-
-        if i % 10_000 == 0 {
-            loss.print()
-        }
-    }
-
-    tch::manual_seed(SEED + 10);
-
-    model.set_training(false);
-    println!("Training loss -> {}", split_loss(xtr, &mut model, &ytr));
-    println!("Validation loss -> {}", split_loss(xdev, &mut model, &ydev));
-
-    // Generate a name by repeatedly sampling from the probability distribution of bigrams
-    // until an end token is found.
-    for _ in 0..20 {
-        let mut out = Vec::new();
-        let mut ctx = tch::Tensor::zeros(CHUNK_SIZE, (Kind::Int, device));
-        loop {
-            let logits = model.forward(ctx.view([1, CHUNK_SIZE]));
-            let prob = logits.softmax(1, Kind::Float);
-            let idx = prob.multinomial(1, true).get(0).int64_value(&[0]);
-            out.push(i_to_c[&(idx as usize)]);
-            if idx == 0 {
-                break;
-            }
-            ctx = tch::Tensor::cat(&[ctx.i(1..), tch::Tensor::from_slice(&[idx])], 0);
-        }
-        println!("{}", out.into_iter().join(""));
-    }
-    Ok(())
+    Sequential { layers }
 }
 
-/// Read all of the names from the dataset
-fn get_names() -> Result<Vec<String>> {
-    Ok(std::fs::read_to_string("names.txt")?
-        .lines()
-        .map(|s| s.to_string())
-        .collect())
-}
-
-/// Create a mapping of char to the integer encoding
-fn create_char_maps(words: &[String]) -> (BTreeMap<char, usize>, HashMap<usize, char>) {
-    let mut c_to_i: BTreeMap<char, usize> = words
-        .join("")
-        .chars()
-        .unique()
-        .sorted()
-        .enumerate()
-        .map(|(i, c)| (c, i + 1))
-        .collect();
-    c_to_i.insert(EDGE_TOKEN, 0);
-    let i_to_c = c_to_i.iter().map(|(c, i)| (*i, *c)).collect();
-    (c_to_i, i_to_c)
-}
-
-fn load_data(
-    words: &[String],
-    char_map: &BTreeMap<char, usize>,
-    block_size: usize,
-) -> (tch::Tensor, tch::Tensor) {
-    let mut xs = vec![];
-    let mut ys = vec![];
-
-    for name in words.iter() {
-        let mut ctx = vec![0_i8; block_size];
-        for c in name.chars().chain(std::iter::once(EDGE_TOKEN)) {
-            let ci = char_map[&c] as i8;
-            xs.push(ctx.clone());
-            ys.push(ci);
-
-            ctx.rotate_left(1);
-            ctx[block_size - 1] = ci;
-        }
-    }
-    (
-        tch::Tensor::from_slice2(&xs).to_kind(Kind::Int),
-        tch::Tensor::from_slice(&ys).to_kind(tch::Kind::Int64),
-    )
-}
-
-fn split_loss(data: tch::Tensor, model: &mut dyn Layer, labels: &tch::Tensor) -> f64 {
-    let _g = tch::no_grad_guard();
-    let logits = model.forward(data);
-    f64::try_from(logits.cross_entropy_for_logits(labels)).unwrap()
+pub(crate) fn create_mlp(
+    device: tch::Device,
+    vocab_size: i64,
+    embedding_size: i64,
+    hidden_layer_size: i64,
+    chunk_size: i64,
+) -> Sequential {
+    let mut layers: Vec<Box<dyn Layer>> = vec![
+        Box::new(Embedding::new(vocab_size, embedding_size, device)),
+        Box::new(FlattenConsecutive { n: chunk_size }),
+        Box::new(Linear::new(
+            embedding_size * chunk_size,
+            hidden_layer_size,
+            false,
+            device,
+        )),
+        Box::new(BatchNorm1D::new(hidden_layer_size, 1e-5, 0.1, device)),
+        Box::new(Tanh::new()),
+    ];
+    let mut last_linear = Linear::new(hidden_layer_size, vocab_size, false, device);
+    tch::no_grad(|| last_linear.weight *= 0.1);
+    layers.push(Box::new(last_linear));
+    Sequential { layers }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,113 @@
-use std::collections::{BTreeMap, HashMap};
+mod layers;
 
+use crate::layers::{create_wavenet, Layer};
 use anyhow::Result;
 use itertools::Itertools;
+use layers::create_mlp;
+use rand::seq::SliceRandom;
+use rand::SeedableRng;
+use rand_chacha::ChaCha12Rng;
+use std::collections::{BTreeMap, HashMap};
+use tch::{IndexOp, Kind};
 
 const EDGE_TOKEN: char = '.';
 const SEED: i64 = 2147483647;
+const EMBEDDING_SIZE: i64 = 24;
+const CHUNK_SIZE: i64 = 8;
+const HIDDEN_LAYER_SIZE: i64 = 128;
+const BATCH_SIZE: i64 = 32;
+const TRAINING_ITERATIONS: i64 = 200_000;
+
+#[derive(Debug)]
+enum ModelType {
+    Wave,
+    Mlp,
+}
 
 fn main() -> Result<()> {
     tch::manual_seed(SEED);
     let device = tch::Device::cuda_if_available();
-    let names = get_names()?;
+    let mut names = get_names()?;
 
     // Generate a mapping of token to integer (and the reverse)
     let (c_to_i, i_to_c) = create_char_maps(&names);
+    let vocab_size = (c_to_i.len() + 1) as i64;
 
-    let (xenc, yenc) = load_bigram_data(&names, &c_to_i);
-    let mut w = tch::Tensor::randn([27, 27], (tch::Kind::Float, device)).requires_grad_(true);
+    // Split dataset into train, dev, test
+    let mut rng = ChaCha12Rng::seed_from_u64(42);
+    names.shuffle(&mut rng);
+    let test_set_size = (names.len() as f32 * 0.9) as usize;
 
-    for _ in 0..200 {
-        let logits = xenc.matmul(&w).exp();
-        let prob = &logits / logits.sum_dim_intlist([1].as_slice(), true, tch::Kind::Float);
-        let regularization = w.pow_tensor_scalar(2).mean(tch::Kind::Float);
-        // Determine the - log likelihood loss
-        let loss = -prob
-            .index(&[
-                Some(tch::Tensor::arange(
-                    xenc.size()[0],
-                    (tch::Kind::Int64, device),
-                )),
-                Some(yenc.shallow_clone()),
-            ])
-            .log()
-            .mean(tch::Kind::Float)
-            + regularization * 0.01;
-        dbg!(&loss);
-        w.zero_grad();
+    let (xtr, ytr) = load_data(&names[0..test_set_size], &c_to_i, CHUNK_SIZE as usize);
+    let (xdev, ydev) = load_data(&names[test_set_size..], &c_to_i, CHUNK_SIZE as usize);
+    // let mut w = tch::Tensor::randn([27, 27], (tch::Kind::Float, device)).requires_grad_(true);
+
+    // for _ in 0..200 {
+    //     let logits = xenc.matmul(&w).exp();
+    //     let prob = &logits / logits.sum_dim_intlist([1].as_slice(), true, tch::Kind::Float);
+    //     let regularization = w.pow_tensor_scalar(2).mean(tch::Kind::Float);
+    //     // Determine the - log likelihood loss
+    //     let loss = -prob
+    //         .index(&[
+    //             Some(tch::Tensor::arange(
+    //                 xenc.size()[0],
+    //                 (tch::Kind::Int64, device),
+    //             )),
+    //             Some(yenc.shallow_clone()),
+    //         ])
+    //         .log()
+    //         .mean(tch::Kind::Float)
+    //         + regularization * 0.01;
+    //     dbg!(&loss);
+    //     w.zero_grad();
+    //     loss.backward();
+    //     tch::no_grad(|| w += w.grad() * -50);
+    // }
+
+    let mut model = create_model(ModelType::Mlp, device, vocab_size);
+
+    for i in 0..TRAINING_ITERATIONS {
+        let ix = tch::Tensor::randint(xtr.size()[0], [BATCH_SIZE], (Kind::Int, device));
+        let samples = xtr.i(&ix);
+        let labels = ytr.i(&ix);
+        let logits = model.forward(samples);
+        let loss = logits.cross_entropy_for_logits(&labels);
+        model.zero_grad();
         loss.backward();
-        tch::no_grad(|| w += w.grad() * -50);
-    }
+        let lr = if i < 100_000 { 0.1 } else { 0.01 };
 
-    tch::manual_seed(SEED);
-    // Generate a name by repeatedly sampling from the probability distribution of bigrams
-    // until an end token is found.
-    let mut idx = 0;
-    let mut out = Vec::new();
-    loop {
-        let xenc = tch::Tensor::from_slice(&[idx])
-            .one_hot(27)
-            .to_kind(tch::Kind::Float);
-        let counts = xenc.matmul(&w).exp();
-        let prob = &counts / counts.sum_dim_intlist([1].as_slice(), true, tch::Kind::Float);
-        idx = prob.multinomial(1, true).get(0).int64_value(&[0]);
-        out.push(i_to_c[&(idx as usize)]);
-        if idx == 0 {
-            break;
+        tch::no_grad(|| model.backward(lr));
+
+        if i % 10_000 == 0 {
+            loss.print()
         }
     }
-    println!("{}", out.into_iter().join(""));
+
+    tch::manual_seed(SEED + 10);
+    model.set_training(false);
+    println!("Training loss -> {}", split_loss(xtr, model.as_mut(), &ytr));
+    println!(
+        "Validation loss -> {}",
+        split_loss(xdev, model.as_mut(), &ydev)
+    );
+
+    // Generate a name by repeatedly sampling from the probability distribution of bigrams
+    // until an end token is found.
+    for _ in 0..20 {
+        let mut out = Vec::new();
+        let mut ctx = tch::Tensor::zeros(CHUNK_SIZE, (Kind::Int, device));
+        loop {
+            let logits = model.forward(ctx.view([1, CHUNK_SIZE]));
+            let prob = logits.softmax(1, Kind::Float);
+            let idx = prob.multinomial(1, true).get(0).int64_value(&[0]);
+            out.push(i_to_c[&(idx as usize)]);
+            if idx == 0 {
+                break;
+            }
+            ctx = tch::Tensor::cat(&[ctx.i(1..), tch::Tensor::from_slice(&[idx])], 0);
+        }
+        println!("{}", out.into_iter().join(""));
+    }
     Ok(())
 }
 
@@ -83,22 +134,46 @@ fn create_char_maps(words: &[String]) -> (BTreeMap<char, usize>, HashMap<usize, 
     (c_to_i, i_to_c)
 }
 
-fn load_bigram_data(
+fn load_data(
     words: &[String],
     char_map: &BTreeMap<char, usize>,
+    block_size: usize,
 ) -> (tch::Tensor, tch::Tensor) {
     let mut xs = vec![];
     let mut ys = vec![];
 
-    for name in words {
-        let edged_name = format!("{}{}{}", EDGE_TOKEN, name, EDGE_TOKEN);
-        for (c1, c2) in edged_name.chars().zip(edged_name.chars().skip(1)) {
-            xs.push(char_map[&c1] as i8);
-            ys.push(char_map[&c2] as i8);
+    for name in words.iter() {
+        let mut ctx = vec![0_i8; block_size];
+        for c in name.chars().chain(std::iter::once(EDGE_TOKEN)) {
+            let ci = char_map[&c] as i8;
+            xs.push(ctx.clone());
+            ys.push(ci);
+
+            ctx.rotate_left(1);
+            ctx[block_size - 1] = ci;
         }
     }
     (
-        tch::Tensor::from_slice(&xs).onehot(27),
-        tch::Tensor::from_slice(&ys).to_kind(tch::Kind::Int64),
+        tch::Tensor::from_slice2(&xs).to_kind(Kind::Int),
+        tch::Tensor::from_slice(&ys).to_kind(Kind::Int64),
     )
+}
+
+fn split_loss(data: tch::Tensor, model: &mut dyn Layer, labels: &tch::Tensor) -> f64 {
+    let _g = tch::no_grad_guard();
+    let logits = model.forward(data);
+    f64::try_from(logits.cross_entropy_for_logits(labels)).unwrap()
+}
+
+fn create_model(model_type: ModelType, device: tch::Device, vocab_size: i64) -> Box<dyn Layer> {
+    Box::new(match model_type {
+        ModelType::Wave => create_wavenet(device, vocab_size, EMBEDDING_SIZE, HIDDEN_LAYER_SIZE),
+        ModelType::Mlp => create_mlp(
+            device,
+            vocab_size,
+            EMBEDDING_SIZE,
+            HIDDEN_LAYER_SIZE,
+            CHUNK_SIZE,
+        ),
+    })
 }
